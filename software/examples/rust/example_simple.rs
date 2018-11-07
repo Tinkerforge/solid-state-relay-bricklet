143358d5512b7f07bb5586bca23e2841befb02da
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:27:41 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,23 +1,23 @@
 use std::{error::Error, io, thread, time::Duration};
-use tinkerforge::{ipconnection::IpConnection, solid_state_relay_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, solid_state_relay_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Solid State Relay Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Solid State Relay Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let solid_state_relay_bricklet = SolidStateRelayBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let ssr = SolidStateRelayBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Turn relay on/off 10 times with 1 second delay
     for i in 0..5 {
         thread::sleep(Duration::from_millis(1000));
-        solid_state_relay_bricklet.set_state(true);
+        ssr.set_state(true);
         thread::sleep(Duration::from_millis(1000));
-        solid_state_relay_bricklet.set_state(false);
+        ssr.set_state(false);
     }
 
     println!("Press enter to exit.");
