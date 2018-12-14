42b901a59d57ad8b59a4cd384bdd505efc237e0d
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:15:53 +0100

    Add go examples.

---
+++ software/examples/go/example_simple.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"tinkerforge/ipconnection"
+	"tinkerforge/solid_state_relay_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Solid State Relay Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	ssr, _ := solid_state_relay_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Turn relay on/off 10 times with 1 second delay
+	for i := 0; i < 5; i++ {
+		time.Sleep(1000 * time.Millisecond)
+		ssr.SetState(true)
+		time.Sleep(1000 * time.Millisecond)
+		ssr.SetState(false)
+	}
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
